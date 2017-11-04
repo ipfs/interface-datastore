07243cbb57c08a59935192ce7f0cfcb5ee6da97d
thisconnect <hi@this-connect.me>
2017-11-04 11:42:07 +0100

    should pass on Node.js v4

--- src/tests.js
+++ src/tests.js
@@ -11,7 +11,7 @@ const series = require('async/series')
 const parallel = require('async/parallel')
 const map = require('async/map')
 const each = require('async/each')
-const { randomBytes } = require('crypto') // TODO: change to 'webcrypto'
+const crypto = require('crypto') // TODO: change to 'webcrypto'
 
 const Key = require('../src').Key
 
@@ -228,9 +228,9 @@ module.exports = (test/* : Test */) => {
       const b = check(store).batch()
       const count = 400
       for (let i = 0; i < count; i++) {
-        b.put(new Key(`/a/hello${i}`), randomBytes(32))
-        b.put(new Key(`/q/hello${i}`), randomBytes(64))
-        b.put(new Key(`/z/hello${i}`), randomBytes(128))
+        b.put(new Key(`/a/hello${i}`), crypto.randomBytes(32))
+        b.put(new Key(`/q/hello${i}`), crypto.randomBytes(64))
+        b.put(new Key(`/z/hello${i}`), crypto.randomBytes(128))
       }
 
       series([
