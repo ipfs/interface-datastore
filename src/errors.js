7f4eabb637c7d50dae82e0bfa404427c8ba88bf5
Alex Potsides <alex@achingbrain.net>
2021-02-05 16:33:31 +0000

    chore: update deps (#73)

--- src/errors.js
+++ src/errors.js
@@ -1,45 +1,45 @@
 'use strict'
 
-const errcode = require('err-code')
+const errCode = require('err-code')
+
 /**
- *
  * @param {Error} [err]
  */
-const dbOpenFailedError = (err) => {
+function dbOpenFailedError (err) {
   err = err || new Error('Cannot open database')
-  return errcode(err, 'ERR_DB_OPEN_FAILED')
+  return errCode(err, 'ERR_DB_OPEN_FAILED')
 }
+
 /**
- *
  * @param {Error} [err]
  */
-const dbDeleteFailedError = (err) => {
+function dbDeleteFailedError (err) {
   err = err || new Error('Delete failed')
-  return errcode(err, 'ERR_DB_DELETE_FAILED')
+  return errCode(err, 'ERR_DB_DELETE_FAILED')
 }
+
 /**
- *
  * @param {Error} [err]
  */
-const dbWriteFailedError = (err) => {
+function dbWriteFailedError (err) {
   err = err || new Error('Write failed')
-  return errcode(err, 'ERR_DB_WRITE_FAILED')
+  return errCode(err, 'ERR_DB_WRITE_FAILED')
 }
+
 /**
- *
  * @param {Error} [err]
  */
-const notFoundError = (err) => {
+function notFoundError (err) {
   err = err || new Error('Not Found')
-  return errcode(err, 'ERR_NOT_FOUND')
+  return errCode(err, 'ERR_NOT_FOUND')
 }
+
 /**
- *
  * @param {Error} [err]
  */
-const abortedError = (err) => {
+function abortedError (err) {
   err = err || new Error('Aborted')
-  return errcode(err, 'ERR_ABORTED')
+  return errCode(err, 'ERR_ABORTED')
 }
 
 module.exports = {
